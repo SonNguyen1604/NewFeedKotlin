0000854103ab77ae291fc0318bf0df85e46283e3
nguyen.ngoc.sonc <nguyen.ngoc.sonc@framgias-Mac-mini-3.local>
2018-06-05 15:04:51 +0700

    Create screen signin/signup

---
+++ app/src/main/java/com/example/nguyenngocsonc/myapplication/signin/NewFeedActivity.kt
@@ -0,0 +1,13 @@
+package com.example.nguyenngocsonc.myapplication.signin
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import com.example.nguyenngocsonc.myapplication.R
+
+class NewFeedActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_new_feed)
+    }
+}
